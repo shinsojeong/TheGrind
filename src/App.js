3e7f01c15daa8815ef4265f0aa4a04dd2f79f9ce
shinsojeong <shinsojeong82@gmail.com>
2023-07-17 22:49:36 +0900

    basename 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './styles/app.css';
 import Header from './components/Header.js';
 import Home from './screen/Home';
@@ -9,12 +9,10 @@ function App() {
     <>
       <Header></Header>
       <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/menu" element={<Menu/>}/>
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+        </Routes>
       </div>
     </>
   );

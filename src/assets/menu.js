398c561860778c498020ce446bfc06661f5589af
shinsojeong <shinsojeong82@gmail.com>
2023-07-21 21:18:19 +0900

    css 수정

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -15,17 +15,17 @@ export const milk = [
   { name: "제주 녹차라떼", src: "제주 녹차라떼.png", price: "6,000" },
   { name: "제주 보리개역", src: "제주 보리개역.png", price: "6,000" },
   { name: "생초코라떼", src: "생초코라떼.png", price: "6,500" },
-  { name: "딸기라떼", src: "딸기라떼.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
+  { name: "딸기라떼", src: "딸기라떼.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
 ];
 
 export const freshJuice = [
-  { name: "수박주스", src: "수박주스.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "키위주스", src: "키위주스.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
+  { name: "수박주스", src: "수박주스.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "키위주스", src: "키위주스.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
 ];
 
 export const tea = [
   { name: "일과리 애플시나몬티", src: "일과리 애플시나몬티.png", price: "6,000" },
-  { name: "리얼 복숭아 아이스티", src: "리얼 복숭아 아이스티.png", price: "6,000", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
+  { name: "리얼 복숭아 아이스티", src: "리얼 복숭아 아이스티.png", price: "6,000", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
   { name: "오미자 블랙티", src: "오미자 블랙티.png", price: "6,000" },
   { name: "청귤차", src: "청귤차.png", price: "5,000" },
   { name: "자몽차", src: "자몽차.png", price: "5,000" },
@@ -36,12 +36,12 @@ export const tea = [
 ];
 
 export const adeAndSmoothie = [
-  { name: "자몽에이드", src: "자몽에이드.png", price: "6,000", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "레몬에이드", src: "레몬에이드.png", price: "6,000", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "패션후르츠에이드", src: "패션후르츠에이드.png", price: "6,000", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "자두 복숭아 칠러", src: "자두 복숭아 칠러.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "자스민 망고 피치 스무디", src: "자스민 망고 피치 스무디.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
-  { name: "망고 요거트 스무디", src: "망고 요거트 스무디.png", price: "6,500", tag: {background: "#050099", color: "white", text: "ONLY ICE"} },
+  { name: "자몽에이드", src: "자몽에이드.png", price: "6,000", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "레몬에이드", src: "레몬에이드.png", price: "6,000", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "패션후르츠에이드", src: "패션후르츠에이드.png", price: "6,000", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "자두 복숭아 칠러", src: "자두 복숭아 칠러.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "자스민 망고 피치 스무디", src: "자스민 망고 피치 스무디.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
+  { name: "망고 요거트 스무디", src: "망고 요거트 스무디.png", price: "6,500", tag: {background: "#003299", color: "white", text: "ONLY ICE"} },
 ];
 
 export const dessert = [

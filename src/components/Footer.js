71059458ccc4335558b6a052b80592caf2c66f42
shinsojeong <shinsojeong82@gmail.com>
2023-07-26 15:43:23 +0900

    footer 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <div id="footer">
-      <p>63504 제주 서귀포시 대정읍 노을해안로 98</p>
-      <p>ⓒTHE GRIND All rights reserved.</p>
+      <p>63504 제주 서귀포시 대정읍 노을해안로 98.</p>
+      <p>ⓒ THE GRIND All rights reserved.</p>
     </div>
   );
 };
